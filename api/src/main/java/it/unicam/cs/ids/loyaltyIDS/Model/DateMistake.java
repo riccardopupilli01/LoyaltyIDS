bf44d483d5a04616fd220793e59482ebbb73feab
andreapalmieri01 <andrea.palmieri2001@gmail.com>
2023-01-23 15:56:16 +0100

    IDS Project gradle (Exceptions)

---
+++ api/src/main/java/it/unicam/cs/ids/loyaltyIDS/Model/DateMistake.java
@@ -0,0 +1,10 @@
+package it.unicam.cs.ids.loyaltyIDS.Model;
+
+public class DateMistake extends Throwable {
+    public DateMistake() {
+    }
+
+    public DateMistake(String message) {
+        super(message);
+    }
+}
