ab7501f883b3fd87ae277af4fd230c77422e4d55
andreapalmieri01 <andrea.palmieri2001@gmail.com>
2023-01-23 15:26:14 +0100

    IDS Project gradle (ProgrammaPunti)

---
+++ api/src/main/java/it/unicam/cs/ids/loyaltyIDS/Model/ProgrammaPunti.java
@@ -0,0 +1,40 @@
+package it.unicam.cs.ids.loyaltyIDS.Model;
+
+
+public class ProgrammaPunti extends ProgrammaFedelta {
+
+    private double valoreXPunto;    //Valore in euro per singolo punto
+    private int totPunti;       //Punti da totalizzare per ottenere coupon
+    private Coupon coupon;      //vantaggio ottenibile al raggiungimento di una certa soglia di punti
+
+    public ProgrammaPunti(String nome, String descrizione,  double valoreXPunto, int totPunti, Coupon coupon) {
+        super(nome, descrizione);
+        this.valoreXPunto= valoreXPunto;
+        this.totPunti= totPunti;
+        this.coupon= coupon;
+    }
+
+    public ProgrammaPunti(String nome, String descrizione) {
+        super(nome, descrizione);
+    }
+
+    public ProgrammaPunti(String nome, int id) {
+        super(nome, id);
+    }
+
+    public ProgrammaPunti(String nome) {
+        super(nome);
+    }
+
+    public double getValoreXPunto() {
+        return valoreXPunto;
+    }
+
+    public int getTotPunti() {
+        return totPunti;
+    }
+
+    public Coupon getCoupon() {
+        return coupon;
+    }
+}
